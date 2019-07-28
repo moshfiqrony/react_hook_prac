d54be2e891dc65a15fcfff4092809154d7283faa
moshfiqrony <moshfiqrony@gmail.com>
2019-07-28 16:17:15 +0600

    useState is implemented

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width: 100, margin: "0 auto"}}>
+      
+      <div style={{width: 50, margin: "0 auto"}}>
+        <h1>{count}</h1>
+        </div>
+      <button onClick={() => {
+        setCount(count+1);
+      }}>Click me </button>
     </div>
   );
 }
