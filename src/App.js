6d71f32f6bb04b728aed41398f549abeef435452
moshfiqrony <moshfiqrony@gmail.com>
2019-07-28 16:30:56 +0600

    multiple useState with button click

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React, { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [val, setVal] = useState(0);
   return (
     <div style={{width: 100, margin: "0 auto"}}>
       
       <div style={{width: 50, margin: "0 auto"}}>
         <h1>{count}</h1>
+        <input name="val" onChange={(e) => {
+            setVal(e.target.value)
+        }} placeholder="Enter Value"/>
         </div>
       <button onClick={() => {
-        setCount(count+1);
+        setCount(parseInt(count)+parseInt(val));
       }}>Click me </button>
     </div>
   );
